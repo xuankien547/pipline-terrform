d2be066409ef552bd96381f3e3ca32fff1f57cc6
Kien <xuankien547@gmail.com>
2025-06-21 23:00:07 +0700

    update[21]

--- terraform-source/module/s3/outputs.tf
+++ terraform-source/module/s3/outputs.tf
@@ -1,4 +1,4 @@
 output "s3_bucket_arn" {
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value       = try(aws_s3_bucket.this[0].arn, "ARN not available")
+  value       = try(aws_s3_bucket.s3_bucket.arn, "ARN not available")
 }
